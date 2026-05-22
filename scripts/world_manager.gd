c007571a318f60c02394c56c944134585ac8a12f
Allan Costa <allaninocencio@yahoo.com.br>
2026-05-22 09:05:45 -0300

    Remove unused chunk_leeway

--- scripts/world_manager.gd
+++ scripts/world_manager.gd
@@ -14,11 +14,6 @@ var _last_player_chunk = Vector2i(9999, 9999)
 var slow_frame_counter: int = 0
 var _shaders_stopped: bool = false
 
-# We increase our chunks by this to make junctions more seamless
-# To test this, look at the right of zeta, the pole has a junction
-# along t = 0.00.
-const chunk_leeway = 0.0
-
 @onready var sun = get_node("../DirectionalLight3D")
 @onready var moon = get_node("../MoonLight")
 @onready var world_environment = get_node("../WorldEnvironment")
@@ -229,7 +224,7 @@ func _get_lod_level(coord: Vector2i, player_coord: Vector2i) -> int:
 
 func _create_lod_mesh(size: float, subdivisions: int) -> Mesh:
 	var plane = PlaneMesh.new()
-	plane.size = Vector2(size + chunk_leeway, size + chunk_leeway)
+	plane.size = Vector2(size, size)
 	plane.subdivide_width = subdivisions
 	plane.subdivide_depth = subdivisions
 	return plane
