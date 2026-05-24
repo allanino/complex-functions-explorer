515c586c7cebfdfe592b1da41255256753c798d0
Allan Costa <allaninocencio@yahoo.com.br>
2026-05-24 18:54:51 -0300

    Improve near resolution

--- scripts/world_manager.gd
+++ scripts/world_manager.gd
@@ -297,29 +297,32 @@ func _update_all_chunks_lod(force: bool = false):
 func _update_lod_subs():
 	match Config.terrain_detail:
 		0: # High
-			LOD_SUBS = [511, 255, 127, 63, 31]
+			LOD_SUBS = [1023, 511, 255, 127, 63, 31]
 		1: # Medium
-			LOD_SUBS = [255, 127, 63, 31, 15]
+			LOD_SUBS = [511, 255, 127, 63, 31, 15]
 		2: # Low
-			LOD_SUBS = [127, 63, 31, 15, 7]
+			LOD_SUBS = [255, 127, 63, 31, 15, 7]
 		3: # Lowest
-			LOD_SUBS = [63, 31, 15, 7, 3]
+			LOD_SUBS = [127, 63, 31, 15, 7, 3]
 
 func _get_lod_level(coord: Vector2i, player_coord: Vector2i) -> int:
 	var dx = abs(coord.x - player_coord.x)
 	var dz = abs(coord.y - player_coord.y)
 	var dist = max(dx, dz)
 
-	if dist <= 1:
+
+	if dist <= 0:
 		return 0
-	elif dist <= 2:
+	if dist <= 1:
 		return 1
-	elif dist <= 4:
+	elif dist <= 2:
 		return 2
-	elif dist <= 6:
+	elif dist <= 4:
 		return 3
-	else:
+	elif dist <= 6:
 		return 4
+	else:
+		return 5
 
 func _create_lod_mesh(size: float, subdivisions: int) -> Mesh:
 	var plane = PlaneMesh.new()
