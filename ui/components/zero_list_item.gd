ada0aaec737a75046ad5e30066b1de309147abd8
Allan Costa <allaninocencio@yahoo.com.br>
2026-06-05 20:16:28 -0300

    style: fix formatting and whitespace in zero_list_item UI components

--- ui/components/zero_list_item.gd
+++ ui/components/zero_list_item.gd
@@ -3,14 +3,14 @@ extends PanelContainer
 @onready var real_label = %RealLabel
 @onready var imag_label = %ImagLabel
 
-var is_active: bool = false : set = set_active
+var is_active: bool = false: set = set_active
 
 func set_active(val: bool):
 	is_active = val
 	var style = get_theme_stylebox("panel").duplicate() as StyleBoxFlat
 	if is_active:
 		style.border_color = Color("#c8a96e")
-		style.bg_color = Color(200.0/255.0, 169.0/255.0, 110.0/255.0, 0.07)
+		style.bg_color = Color(200.0 / 255.0, 169.0 / 255.0, 110.0 / 255.0, 0.07)
 	else:
 		style.border_color = Color(1, 1, 1, 0)
 		style.bg_color = Color(0.909804, 0.894118, 0.862745, 0.08)
@@ -18,4 +18,4 @@ func set_active(val: bool):
 
 func set_values(re: String, im: String):
 	real_label.text = "1/2" if re == "0.500" else re
-	imag_label.text = "+ " + im + " i"
+	imag_label.text = " + " + im + " i"
