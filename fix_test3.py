a098be0784b5b113870b94932e274dcc41028ac4
Allan Costa <allaninocencio@yahoo.com.br>
2026-06-10 08:59:59 -0300

    refactor: remove obsolete test diagnostic scripts and local test runner

--- fix_test3.py
+++
@@ -1,5 +0,0 @@
-with open('tests/test_complex_field.gd', 'r') as f:
-    content = f.read()
-
-# Make sure we restore Config properly if any assert fails
-# Actually gut assertions don't throw exceptions, they just print and continue, so the restore at the end should run.
