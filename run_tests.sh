43dd971d764a9a41d65008718aad136ca39bcb78
Allan Costa <allaninocencio@yahoo.com.br>
2026-06-11 13:21:37 -0300

    Improve CI to catch compilation errors and fail more often

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,14 @@
-godot --headless -s addons/gut/gut_cmdln.gd -gdir=res://tests
+#!/bin/bash
+set -o pipefail
+
+godot \
+--headless \
+-s addons/gut/gut_cmdln.gd \
+-gdir=res://tests \
+-ginclude_subdirs \
+-glog=1 \
+-gexit \
+2>&1 | tee gut.log
+
+
+grep -i -E "SCRIPT ERROR|FAILED|ERROR|NOT OK|WARNING|ORPHANED|CRASHED|INVALID" gut.log && exit 1
