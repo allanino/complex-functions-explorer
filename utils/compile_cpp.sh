7cc34335a0991e006f984b18c7f7e8118b511aba
Allan Costa <allaninocencio@yahoo.com.br>
2026-06-13 13:52:50 -0300

    feat: update build script to support conditional release compilation via CLI arguments

--- utils/compile_cpp.sh
+++ utils/compile_cpp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
-scons platform=linux target=template_debug
-scons platform=linux target=template_release
-
-scons platform=windows target=template_debug
-scons platform=windows target=template_release
+if [[ "$1" == "release" || "$1" == "-r" || "$1" == "--release" ]]; then
+    scons platform=linux target=template_debug
+    scons platform=linux target=template_release
+    scons platform=windows target=template_release
+else
+    scons platform=linux target=template_debug
+fi
