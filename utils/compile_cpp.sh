4cc2df16d1040cae6d10956aea6d01d1302f6279
Allan Costa <allaninocencio@yahoo.com.br>
2026-06-13 13:37:01 -0300

    feat: add project documentation and extend build system for Windows targets

--- utils/compile_cpp.sh
+++ utils/compile_cpp.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+
 scons platform=linux target=template_debug
 scons platform=linux target=template_release
+
+scons platform=windows target=template_debug
+scons platform=windows target=template_release
